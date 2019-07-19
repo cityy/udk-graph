ab347dc5dca485226566b1e432b87376338871c6
cityy <ferdinand.list@googlemail.com>
2019-07-19 21:49:26 +0200

    Basic filtering by node labels, filters childNodes on hiding, it's possible to search filters via the input

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@
 // 1. includes
 // 2. requester object
 // 3. graph object
-// 4. window.onload
+// 4. filter object
+// 5. window.onload
 
 
 //
@@ -65,42 +66,92 @@ const requester = {
 //
 const graph = {
     obj: undefined,
-    hiddenNodes:[],
-    hiddenRelations:[],
+    hidden:{
+        nodes:[],
+        links:[], // called relations in neo4J
+    },
     options: { /* controlType and renderConfig */ 
-        controlType: 'orbit'
+        controlType: 'orbit',
     },
     colors: {
 
     },
-    getNodesBy: function(prop, value){
-        // todo: differentiate between visible and hidden
+    getNodesBy: function(prop, value, includeChildNodes = true){
         let graphData = graph.obj.graphData(); 
-        let results = {};
+        let results = {
+            visible:[],
+            hidden:[],
+        };
+        // look in visible nodes
         for(let i=0;i<graphData.nodes.length;i++){
             if(graphData.nodes[i][prop] == value){
-                results[graphData.nodes[i].id] = graphData.nodes[i];
+                results.visible.push(graphData.nodes[i]);
+                // console.log(graphData.nodes[i]);
+            }
+        }
+        // look in hidden nodes
+        for(let j=0; j<graph.hidden.nodes.length;j++){
+            if(graph.hidden.nodes[j][prop] == value){
+                results.hidden.push(graph.hidden.nodes[j]);
+            }  
+        }
+
+        // console.log(results);
+        if(includeChildNodes){
+            for(let k=0; k<results.visible.length;k++){
+                for(let l=0; l<graphData.links.length;l++){
+                    if(graphData.links[l].target === results.visible[k]){
+                        results.visible.push(graphData.links[l].source);
+                    }
+                }
+            }
+            for(let m=0; m<results.hidden.length;m++){
+                for(let n=0; n<graph.hidden.links.length;n++){
+                    if(graph.hidden.links[n].target === results.visible[m]){
+                        results.visible.push(graphData.links[n].source);
+                    }
+                }
             }
         }
         return results;
     },
     hideNodes: function(nodes){
-        // todo: store the newly hidden nodes and relations
+        console.log(nodes);
         let newGraphData = graph.obj.graphData();
-        for(let id in nodes){
-            newGraphData.links = newGraphData.links.filter(l => l.source !== nodes[id] && l.target !== nodes[id]); // Remove links attached to node
-            let index = newGraphData.nodes.indexOf(nodes[id]);
+        for(let i = 0; i < nodes.length; i++){
+            // store links that will be hidden (filter returns an array of links for each node to be hidden)
+            let linksToHide = newGraphData.links.filter(l => l.source === nodes[i] || l.target === nodes[i]);
+            for(let i=0; i<linksToHide.length;i++){
+                graph.hidden.links.push(linksToHide[i]);
+            }
+            // filter links to hide from the graph
+            newGraphData.links = newGraphData.links.filter(l => l.source !== nodes[i] && l.target !== nodes[i]); // Remove links attached to node
+            // get the index of the current node to hide in the graph data array and remove it
+            let index = newGraphData.nodes.indexOf(nodes[i]);
             newGraphData.nodes.splice(index, 1);
+            // store the newly hidden node
+            graph.hidden.nodes.push(nodes[i]);
+            console.log(document.getElementById(nodes[i].label))
+            document.getElementById(nodes[i].label).checked = false;
         }
         graph.obj.graphData(newGraphData);  
     },
     showNodes:function(nodes){
         let newGraphData = graph.obj.graphData();
-        for(let id in nodes){
-            newGraphData.nodes.push(nodes)
-            // todo: get the hidden relations according to the nodes
-            newGraphData.links.push(nodes)
+        let restoreLinks = [];
+        for(let i = 0; i < nodes.length; i++){
+            graph.hidden.nodes.splice( graph.hidden.nodes.indexOf(nodes[i]), 1 );
+            newGraphData.nodes.push(nodes[i]);
+            let linksToShow = graph.hidden.links.filter(l => { 
+                return (l.source === nodes[i] || l.target === nodes[i]) && ( graph.hidden.nodes.indexOf(l.source) === -1 && graph.hidden.nodes.indexOf(l.target) === -1 );
+            });
+            for(let i=0; i<linksToShow.length;i++){
+                graph.hidden.links.splice( graph.hidden.links.indexOf(linksToShow[i]), 1);
+                newGraphData.links.push(linksToShow[i]);
+            }
         }
+        // console.log(graph.hidden);
+        graph.obj.graphData(newGraphData);
     },
     init: function(data){
         var black = 'rgba(0,0,0,1)';
@@ -123,7 +174,7 @@ const graph = {
                 target: data[0].relations[i].end.low,
                 type: data[0].relations[i].type,
             };
-        } // relations
+        } // links
 
 
         function getRandomColor() {
@@ -139,6 +190,8 @@ const graph = {
                 graph.colors[data[0].labels[i]] = getRandomColor();
         } // labels
         const NODE_REL_SIZE = 4;
+        let highlightNodes = [];
+        let highlightLinks = [];
         graph.obj = ForceGraph3d(graph.options)(document.getElementById('graph'))
             // general
             .showNavInfo(false)
@@ -146,32 +199,90 @@ const graph = {
             .dagLevelDistance(100)
             .graphData(myData)
             .backgroundColor('#101020')
-            // .backgroundColor('#ffffff')
             // nodes
-            .nodeColor( node => graph.colors[node.label] )
-            // .nodeAutoColorBy('type')
-            // .nodeThreeObjectExtend(true)
-            // .nodeThreeObject(node => {
-            //     const sprite = new SpriteText(`${node.name}`);
-            //     sprite.color = black;
-            //     sprite.textHeight = 4;
-            //     return sprite;
-            // })
+            .nodeColor( node => highlightNodes.indexOf(node) === -1 ? graph.colors[node.label] : 'rgba(255,0,0,1)' )
             // links
-            // .linkColor([grey])
-            .linkWidth("1px")
+            .linkWidth(link => highlightLinks.indexOf(link) === -1 ? 1 : 4)
             // particles
-            .linkDirectionalParticles(2)
+            .linkDirectionalParticles(link => highlightLinks.indexOf(link) === -1 ? 2 : 4)
             .linkDirectionalParticleWidth(0.8)
             .linkDirectionalParticleSpeed(0.006)
             .d3Force('collision', d3.forceCollide(node => Math.cbrt(node.size) * NODE_REL_SIZE).radius(10))
-            .d3VelocityDecay(0.3);
+            .d3VelocityDecay(0.3)
+            // interaction
+            .onNodeHover(node => {
+                // no state change
+                if ((!node && !highlightNodes.length) || (highlightNodes.length === 1 && highlightNodes[0] === node)) return;
+                highlightNodes = node ? [node] : [];
+                highlightLinks = [];
+                let graphData = graph.obj.graphData();
+                for(let i=0; i<graphData.links.length; i++){
+                    if(graphData.links[i].target === node){
+                        highlightLinks.push(graphData.links[i]);
+                        highlightNodes.push(graphData.links[i].source);
+                    }
+                }
+                updateGeometries();
+            })
+            .onLinkHover(link => {
+                // no state change
+                if ((!link && highlightLinks.length) || (highlightLinks.length === 1 && highlightLinks[0] === link)  ) return;
+                  highlightLinks = [];
+                  highlightLinks.push(link);
+                  highlightNodes = link ? [link.source, link.target] : [];
+                updateGeometries();
+            });
+            function updateGeometries() {
+              graph.obj.nodeRelSize(4); // trigger update of 3d objects in scene
+            }
     } //</f: init>
 };
 
 const filter = {
     checklist: document.getElementById("filterCheckList"),
+    allBox: undefined,
     boxes:[],
+    status: {
+        checked: 0,
+        unchecked: 0,
+    },
+    search:function(text){
+        console.log(text);
+        for(let i=0;i<filter.boxes.length;i++){
+            let id = filter.boxes[i].getAttribute('id').toLowerCase();
+            if(id.includes(text) || id.includes(text.toLowerCase())){
+                filter.boxes[i].parentElement.style.display="block";
+            }
+            else{
+                filter.boxes[i].parentElement.style.display="none";
+            }
+        }
+    },
+    apply: function(targetFilter){ // applies a filter when un/checking a checkbox
+                    let filterNodes = graph.getNodesBy("label", targetFilter.getAttribute("id"));
+                    if(targetFilter.checked){ 
+                        graph.showNodes(filterNodes.hidden)
+                        filter.status.checked += 1;
+                        filter.status.unchecked -= 1;
+                    }
+                    else{
+                        graph.hideNodes(filterNodes.visible);
+                        filter.status.checked -= 1;
+                        filter.status.unchecked += 1;
+                    }
+                    if(filter.status.unchecked === 0){
+                        filter.allBox.checked = true;
+                        filter.allBox.classList.remove("varies");
+                    }
+                    else if(filter.status.unchecked < filter.boxes.length){
+                        filter.allBox.checked = true;
+                        filter.allBox.classList.add("varies");
+                    }
+                    else if(filter.status.unchecked === filter.boxes.length){
+                        filter.allBox.checked = false;
+                        filter.allBox.classList.remove("varies");
+                    }
+    },
     init: function(filters){
         filters =  Array.from(new Set(filters.flat())); // flatten array and remove duplicates, fix this in the server request!
         for(let i=0;i<filters.length;i++){
@@ -184,32 +295,45 @@ const filter = {
             input.setAttribute("checked", "");
             let span = document.createElement("span");
             span.innerHTML = filters[i];
-            // change event listener
+            // change event listener for checkboxes
             input.addEventListener("change", (e) => {
                     e.stopPropagation();
                     let target = e.target; 
-                    console.log(target.getAttribute("id"));
-                    if(target.checked){ 
-                        // let filterNodes = graph.getNodesBy("label", target.getAttribute("id"));
-                        // graph.hideNodes(filterNodes);
-                    }
-                    else{
-                        let filterNodes = graph.getNodesBy("label", target.getAttribute("id"));
-                        let filterIds = []
-                        for(let ids in filterNodes){
-                            filterIds.push(ids);
-                        }
-                        graph.hideNodes(filterNodes);
-                    }
+                    filter.apply(target);
             });
             // append new dom elements to tree
             li.appendChild(input);
             li.appendChild(span);
             filter.checklist.appendChild(li);
             // store a list of the filter checkboxes
-            filter.boxes.push(input); 
-            //
-        }
+            filter.boxes.push(input)
+        } // </for>
+        // set up the checkbox to un/check all filters
+        filter.allBox = document.getElementById("allLabels");
+        filter.allBox.addEventListener("change", (e) => {
+            e.stopPropagation();
+            let target = e.target;
+            if(target.checked){
+                for(let i=0; i<filter.boxes.length; i++){
+                    filter.boxes[i].checked = true;
+                    filter.boxes[i].dispatchEvent(new Event('change'));
+                }
+                filter.status.unchecked = 0;
+            }
+            else{
+                for(let i=0; i<filter.boxes.length; i++){
+                    filter.boxes[i].checked = false;
+                    filter.boxes[i].dispatchEvent(new Event('change'));
+                }
+                filter.status.unchecked = filter.boxes.length;
+            }
+            filter.allBox.classList.remove("varies");
+        });
+        // set up the filter search input
+        document.getElementById('filterSearchInput').addEventListener('input', (e) => {
+                let text = e.target.value;
+                filter.search(text);
+            });
     }
 }
 
